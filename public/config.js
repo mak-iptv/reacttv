e242217d3ffef1475048ec87c98e771261cc947d
mak-iptv <49493392+mak-iptv@users.noreply.github.com>
2026-02-15 10:21:30 +0200

    Create config.js

---
+++ public/config.js
@@ -0,0 +1,14 @@
+/*----- Player name -----*/
+window.playername = "My Xtream Player";
+
+/*----- DNS -----*/
+window.dns = ""; // DNS do të vendoset nga user
+
+/*----- CORS -----*/
+window.cors = false; // true nëse provider nuk lejon Access-Control-Allow-Origin: *
+
+/*----- HTTPS -----*/
+window.https = false;
+
+/*----- TMDB API [OPTIONAL] -----*/
+window.tmdb = "";
